f4a0015ef4dbfb570fbf3553e972146f447c93fa
Neyork <99612597+Neyork@users.noreply.github.com>
2022-03-16 14:45:33 +0200

    Add files via upload

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,12 +1,11 @@
 #
 # Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-
-# Kanged By © @iNeyork
-# Rocks © @NeyorkSupport
-# Owner Neyork 
-# Neyork
-# All rights reserved. Yukki
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
 
 import os
 from typing import List
@@ -32,9 +31,19 @@ for filename in os.listdir(r"./strings"):
             open(r"./strings/" + filename, encoding="utf8")
         )
 
+
 for filename in os.listdir(r"./strings/langs/"):
+    if "en" not in languages:
+        languages["en"] = yaml.safe_load(
+            open(r"./strings/langs/en.yml", encoding="utf8")
+        )
     if filename.endswith(".yml"):
         language_name = filename[:-4]
+        if language_name == "en":
+            continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
+        for item in languages["en"]:
+            if item not in languages[language_name]:
+                languages[language_name][item] = languages["en"][item]
